a4c1af6b6fdf9dbbf2f4093570c6a5246cf05991
kiki-kanri <a470666@gmail.com>
2025-10-03 14:32:28 +0800

    refactor: chore: tidy up `src/extensions/anyhow.rs`

--- src/extensions/anyhow.rs
+++ src/extensions/anyhow.rs
@@ -1,18 +1,20 @@
 use anyhow::{anyhow, Result};
 use std::fmt::Debug;
 
+// OptionAnyhowExt
 pub trait OptionAnyhowExt<T> {
     fn ok_anyhow(self, msg: impl AsRef<str>) -> Result<T>;
-}
-
-pub trait ResultAnyhowExt<T> {
-    fn map_anyhow(self) -> Result<T>;
 }
 
 impl<T> OptionAnyhowExt<T> for Option<T> {
     fn ok_anyhow(self, msg: impl AsRef<str>) -> Result<T> {
         self.ok_or_else(|| anyhow!("{}", msg.as_ref()))
     }
+}
+
+// ResultAnyhowExt
+pub trait ResultAnyhowExt<T> {
+    fn map_anyhow(self) -> Result<T>;
 }
 
 impl<T, E: Debug> ResultAnyhowExt<T> for Result<T, E> {
