c728c73f8083dff64059e2e5cc127b3439bc4350
kiki-kanri <a470666@gmail.com>
2025-10-12 04:09:24 +0800

    refactor: modify `wait_for_shutdown_signal` to merge Unix-related blocks and avoid importing `select` on non-Unix platforms

--- src/signal.rs
+++ src/signal.rs
@@ -2,27 +2,35 @@ use anyhow::{
     Error,
     Result,
 };
-use tokio::{
-    select,
-    signal,
-};
+use tokio::signal::ctrl_c;
 
 pub async fn wait_for_shutdown_signal() -> Result<()> {
-    let ctrl_c = async { Ok::<(), Error>(signal::ctrl_c().await?) };
-    #[cfg(unix)]
-    let sigterm = async {
-        let mut term_signal = signal::unix::signal(signal::unix::SignalKind::terminate())?;
-        term_signal.recv().await;
-        Ok::<(), Error>(())
-    };
+    let ctrl_c = async { Ok::<(), Error>(ctrl_c().await?) };
 
     #[cfg(unix)]
-    select! {
-        _ = ctrl_c => {},
-        _ = sigterm => {},
+    {
+        use tokio::{
+            select,
+            signal::unix::{
+                signal,
+                SignalKind,
+            },
+        };
+
+        let sigterm = async {
+            let mut term_signal = signal(SignalKind::terminate())?;
+            term_signal.recv().await;
+            Ok::<(), Error>(())
+        };
+
+        select! {
+            _ = ctrl_c => {},
+            _ = sigterm => {},
+        }
     }
 
     #[cfg(not(unix))]
     ctrl_c.await?;
+
     Ok(())
 }
