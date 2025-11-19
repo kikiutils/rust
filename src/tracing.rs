2d6c88d3213f6c192be700944136bd1a8af74dbc
kiki-kanri <a470666@gmail.com>
2025-11-19 17:39:55 +0800

    feat: split `fmt` creation from `init_tracing_with_local_time_format` into separate function returning a layer; add `init_tracing_with_layer` and `make_tracing_fmt_layer_with_local_time`

--- src/tracing.rs
+++ src/tracing.rs
@@ -1,14 +1,35 @@
-use anyhow::Result;
+use anyhow::{
+    Ok,
+    Result,
+};
 use time::{
     macros::format_description,
     UtcOffset,
 };
-use tracing_subscriber::fmt::time::OffsetTime;
+use tracing_subscriber::{
+    fmt::{
+        layer,
+        time::OffsetTime,
+    },
+    layer::SubscriberExt,
+    registry,
+    util::SubscriberInitExt,
+    Layer as TraitLayer,
+    Registry,
+};
+
+pub fn init_tracing_with_layer<L: TraitLayer<Registry> + Send + Sync + 'static>(layer: L) -> Result<()> {
+    registry().with(layer).try_init()?;
+    Ok(())
+}
 
 pub fn init_tracing_with_local_time_format() -> Result<()> {
+    init_tracing_with_layer(make_tracing_fmt_layer_with_local_time()?)
+}
+
+pub fn make_tracing_fmt_layer_with_local_time() -> Result<impl TraitLayer<Registry>> {
     let local_time_offset = UtcOffset::current_local_offset()?;
     let tracing_time_format = format_description!("[year]-[month]-[day] [hour]:[minute]:[second].[subsecond]");
     let tracing_timer = OffsetTime::new(local_time_offset, tracing_time_format);
-    tracing_subscriber::fmt().with_timer(tracing_timer).init();
-    Ok(())
+    Ok(layer().with_timer(tracing_timer))
 }
