95b7641674470fa5a6b52f6d1f468c724b753018
kiki-kanri <a470666@gmail.com>
2025-11-19 17:56:30 +0800

    chore: lint code

--- src/tracing.rs
+++ src/tracing.rs
@@ -25,6 +25,11 @@ use tracing_subscriber::{
     Registry,
 };
 
+// Types
+type WithLocalTimeLayer =
+    Layer<Registry, DefaultFields, Format<Full, OffsetTime<&'static [BorrowedFormatItem<'static>]>>>;
+
+// Functions
 pub fn init_tracing_with_layer<L: TraitLayer<Registry> + Send + Sync + 'static>(layer: L) -> Result<()> {
     registry().with(layer).try_init()?;
     Ok(())
@@ -34,8 +39,7 @@ pub fn init_tracing_with_local_time_format() -> Result<()> {
     init_tracing_with_layer(make_tracing_fmt_layer_with_local_time()?)
 }
 
-pub fn make_tracing_fmt_layer_with_local_time(
-) -> Result<Layer<Registry, DefaultFields, Format<Full, OffsetTime<&'static [BorrowedFormatItem<'static>]>>>> {
+pub fn make_tracing_fmt_layer_with_local_time() -> Result<WithLocalTimeLayer> {
     let local_time_offset = UtcOffset::current_local_offset()?;
     let tracing_time_format = format_description!("[year]-[month]-[day] [hour]:[minute]:[second].[subsecond]");
     let tracing_timer = OffsetTime::new(local_time_offset, tracing_time_format);
