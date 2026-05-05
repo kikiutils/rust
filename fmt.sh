2a9ad8c32a22e386f764cde01e7c64a5e908e092
kiki-kanri <a470666@gmail.com>
2026-05-05 13:01:02 +0800

    chore: remove fmt script and update cargo alias

--- fmt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if ! cargo +nightly fmt --version >/dev/null 2>&1; then
-    rustup toolchain install nightly --component rustfmt --profile minimal
-fi
-
-cargo +nightly fmt --all -- "$@"
